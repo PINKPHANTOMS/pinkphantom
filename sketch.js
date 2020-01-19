7861256b47fa827f5d92c86b002c89ecd36a9826
PINKPHANTOMS <55456019+PINKPHANTOMS@users.noreply.github.com>
2020-01-19 00:49:56 -0800

    .

--- sketch.js
+++ sketch.js
@@ -2,41 +2,39 @@ let xoff = 0.0;
 let noiseScale=0.02;
 let img;
 
+
 function preload(){
 
-      img = loadImage('images/albumart/hh.png')
+      img = loadImage('images/pinky.gif')
 
 
 }
 
 
 function setup() {
-  var canvas = createCanvas(windowWidth, windowHeight, WEBGL);
- canvas.position(0,0);
+  var canvas = createCanvas(windowWidth, 500, WEBGL);
 
- canvas.style("z-index",'-1')
-   canvas.parent('sketch');
 
 }
 function draw() {
    background(0)
-  xoff = xoff + 0.01;
-  let n = noise(xoff) * mouseX/10;
+  xoff = xoff + 0.04;
+  let n = noise(xoff) * mouseX;
   texture(img)
   rotateX(millis()/1000000*n)
     rotateZ(millis()/1000000*n)
-    translate(n,n-mouseY,n+mouseY)
+    // translate(n,n-mouseY,n+mouseY)
 
-  box(n, 0, n, mouseY);
+  box(300);
 
     rotateX(millis()/1000000*n/3)
     rotateZ(millis()/1000000*n/2)
-    translate(n+mouseX,n+mouseY,n-mouseY)
-  box(n, 0, n, mouseY);
+    // translate(n+mouseX,n+mouseY,n-mouseY)
+  box(300);
     rotateX(millis()/1000000*n/3)
     rotateZ(millis()/1000000*n/2)
-    translate(n+mouseY,n+mouseX,n-mouseX)
-    box(n, 0, n, mouseX);
+    // translate(n+mouseY,n+mouseX,n-mouseX)
+    box(300);
 
 
 
