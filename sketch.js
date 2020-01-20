4b8ac655a7b92ec98637e1f441d3315f4f88f776
PINKPHANTOMS <55456019+PINKPHANTOMS@users.noreply.github.com>
2020-01-20 15:49:13 -0800

    hv

--- sketch.js
+++ sketch.js
@@ -1,59 +1,87 @@
 let xoff = 0.0;
 let noiseScale=0.02;
-let vid;
+let img;
 let canvas;
-let graphics;
+var graphics;
+let vid;
+let x = 0
+let value = false;
+var song;
+let dontloop = false;
+
 
 function preload(){
 
-   let vid = createVideo("images/pinky.mp4");
-      vid.play();
 
+
+    img = loadImage('images/pinkphantoms.png')
+    vid = createVideo('images/glitch1.mp4')
+    song = loadSound("images/thisdot.mp3")
 
 }
+
 
 function setup() {
 
+  vid.hide()
+  vid.loop()
+
+
+
   canvas = createCanvas(windowWidth, windowHeight, WEBGL);
-  graphics = createGraphics(200,200)
-
-  vid.loop();
-  vid.hide();
-
+  graphics = createGraphics(675,675)
 }
 
-// function windowResized() {
-//   resizeCanvas(windowWidth, windowHeight);
-// }
 
-// function draw() {
+function draw() {
 
-// graphics.background(img)
+  if(value == true){
+if(dontloop ==false){
+  dontloop = true;
+      song.play();
+}
 
-//   background(0)
-//   xoff = xoff + 0.02;
-//   let n = noise(xoff) * mouseX;
-//   texture(graphics)
-//   rotateX(millis()/1000000*n)
-//     rotateZ(millis()/1000000*n)
-//     // translate(n,n-mouseY,n+mouseY)
-
-//   box(300);
-
-//     rotateX(millis()/1000000*n/3)
-//     rotateZ(millis()/1000000*n/2)
-//     // translate(n+mouseX,n+mouseY,n-mouseY)
-//   box(300);
-//     rotateX(millis()/1000000*n/3)
-//     rotateZ(millis()/1000000*n/2)
-//     // translate(n+mouseY,n+mouseX,n-mouseX)
-//     box(300);
+  texture(vid);
+  rotateX(.4)
+image(vid,-1*(width), -1*(height))  
+box(300);
 
 
+  // graphics.background(vid);
 
-//     for (let x=0; x < width; x++) {
-//     let noiseVal = noise((mouseX+x)*noiseScale, mouseY*noiseScale);
-//     stroke(noiseVal*255);
-//     // line(x, mouseY+noiseVal*80, x, height);
-//   }
-// }
+  xoff = xoff + 0.02;
+  let n = noise(xoff) * (mouseX);
+  rotateX(millis()/1000000)
+    rotateZ(millis()/1000000)
+    // translate(n,n-mouseY,n+mouseY)
+
+
+    rotateY(millis()/1000000)
+    rotateZ(millis()/1000000)
+    translate(34,34,34)
+    // translate(n+mouseX,n+mouseY,n-mouseY)
+  box(100, 60);
+    rotateX(millis()/1000000)
+    rotateY(millis()/1000000)
+    // translate(n+mouseY,n+mouseX,n-mouseX)
+    box(200);
+
+
+
+    for (let x=0; x < width; x++) {
+    let noiseVal = noise((mouseX+x)*noiseScale, mouseY*noiseScale);
+    stroke(noiseVal*255);
+    line(x, mouseY+noiseVal*80, x, height);
+  }
+}
+}
+
+function mousePressed() {
+  value = true;
+  vid.play()
+  if(value==true && dontloop == true){
+    song.stop()
+    value = false;
+    dontloop = false;
+  }
+}
