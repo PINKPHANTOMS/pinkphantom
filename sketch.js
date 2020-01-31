706b2471b5536ea7611f60d32c4de74177c98611
PINKPHANTOMS <55456019+PINKPHANTOMS@users.noreply.github.com>
2020-01-30 22:36:52 -0800

    lkh'

--- sketch.js
+++ sketch.js
@@ -13,7 +13,7 @@ let dontloop = false;
 function preload(){
 
     img = loadImage('images/pinkphantoms.png')
-    vid = createVideo('images/glitch1.mp4')
+    vid = createVideo('images/particle1.mov')
     song = loadSound("images/thisdot.mp3")
 
 }
@@ -32,45 +32,77 @@ function setup() {
 
 
 function draw() {
-
   if(value == true){
 if(dontloop ==false){
   dontloop = true;
       song.play();
 }
 
+strokeWeight(0)
+
+background(0)
   texture(vid);
-  rotateX(.4)
-image(vid,-1*(width), -1*(height))  
+// translate(0,0,900)
+  plane(1600,1600)
+
+// translate(0,0,-900)
+  texture(vid);
+
+  rotateX(millis()/10000)
+  rotateY(millis()/10000)
 box(300);
+
+
+// sphere(1000)  
+
 
 
   // graphics.background(vid);
 
   xoff = xoff + 0.02;
-  let n = noise(xoff) * (mouseX);
-  rotateX(millis()/1000000)
-    rotateZ(millis()/1000000)
+  let n = noise(xoff) * (100);
+  rotateX(millis()/10000000)
+    rotateZ(millis()/10000000)
     // translate(n,n-mouseY,n+mouseY)
+translate(0,0,n)
+
+    rotateY(millis()/10000000)
+    rotateZ(millis()/10000000)
+    translate(0,0,400)
+  box(100);
+    translate(0,0,-800)
+
+  translate(0,0,-2*n)
+
+    box(100);
 
 
-    rotateY(millis()/1000000)
-    rotateZ(millis()/1000000)
-    translate(34,34,34)
-    // translate(n+mouseX,n+mouseY,n-mouseY)
-  box(100, 60);
-    rotateX(millis()/1000000)
-    rotateY(millis()/1000000)
-    // translate(n+mouseY,n+mouseX,n-mouseX)
-    box(200);
+        translate(0,400,0)
+  cone(100, 100);
+    translate(0,-800,0)
+    rotateX(millis()/10000000)
+
+  translate(0,-2*n,-4*n)
+
+    cylinder(100,100);
+
+
+        translate(400,0,0)
+  sphere(100);
+    translate(-800,0,0)
+    rotateY(millis()/10000000)
+
+  translate(-4*n,0,-2*n)
+
+    sphere(100);
 
 
 
-    for (let x=0; x < width; x++) {
-    let noiseVal = noise((mouseX+x)*noiseScale, mouseY*noiseScale);
-    stroke(noiseVal*255);
-    line(x, mouseY+noiseVal*80, x, height);
-  }
+  //   for (let x=0; x < width; x++) {
+  //   let noiseVal = noise((mouseX+x)*noiseScale, mouseY*noiseScale);
+  //   stroke(noiseVal*255);
+  //   line(x, mouseY+noiseVal*80, x, height);
+  // }
 }
 }
 
@@ -81,5 +113,7 @@ function mousePressed() {
     song.stop()
     value = false;
     dontloop = false;
+    background(0)
+    vid.stop()
   }
 }
