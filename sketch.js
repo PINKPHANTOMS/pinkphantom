f7024da7c90ec2b0b11937f050fa4bf114ee8241
PINKPHANTOMS <55456019+PINKPHANTOMS@users.noreply.github.com>
2020-12-29 21:08:29 -0700

    lk

--- sketch.js
+++ sketch.js
@@ -1,8 +1,27 @@
 let song;
 let playIcon;
 let pauseIcon;
-let mainFont
-let subFont
+let mainFont;
+let subFont;
+let timeStamp;
+
+let bx;
+let by;
+let alignLeft;
+let alignRight;
+let small = 15;
+let boxSize = small;
+let big =  25;
+let overBox = false;
+let locked = false;
+let xOffset = 0.0;
+let yOffset = 0.0;
+let opacity = .7;
+let opacityBoi = true;
+let fadeAway = false;
+
+let aboveSongTitle;
+
 
 function preload(){
 
@@ -15,52 +34,151 @@ function preload(){
 }
 
 function setup() {
+
+  alignLeft = windowWidth*.06896;
+  alightRight = windowWidth*.931;
+  bx = width / 2.0;
+  by = windowHeight*.84;
+
+  aboveSongTitle = windowHeight*(2/3)
+
+  rectMode(RADIUS);
+  strokeWeight(2);
   createCanvas(windowWidth, windowHeight);
 
+
 }
 
-function duration(){
-  return double;
-}
+// function circleMove(){
+//   map(timeStamp, 0, song.duration(), 0, 100, true)
+//   if(song.isPlaying){
+//     timeStamp++
+//
+//   }
+// }
+
 
 function draw() {
 
-  background(0,0,0,.5);
+
+// if(fadeAway){
+// opacity = 0;
+// }else{
+//   opacity = .7
+// }
+//   fill(255,255,255,opacity)
+//   rect(0,0, 50, 50);
 
 
-  textFont(mainFont);
-  textSize(windowWidth/10)
-  fill(255, 255, 255)
-  text("feed me", windowWidth*.05, windowHeight*.8-(windowWidth/15));
+  if(opacityBoi == true){
 
-  textFont(subFont);
-  textSize(windowWidth/20)
-  fill(128, 128, 128)
-  text("pink phantom", windowWidth*.05, windowHeight*.8);
+    background(0,0,0);
 
-  textFont(subFont);
-  textSize(windowWidth/20)
-  fill(128, 128, 128)
-  text(song.duration(), windowWidth*.05, windowHeight*.85);
+    fadeAway = false;
 
-  fill(128, 128, 128, .5)
-  rect(.05*windowWidth, .81*windowHeight, .9*windowWidth, windowWidth*.015);
-// circle()
+    fill(128);
+    rect(windowWidth*.5, windowHeight*.84, windowWidth*.44, 3, 10);
 
-  if(song.isPlaying()){
-    image(pauseIcon, (windowWidth/2)-100, windowHeight-300, 200, 200);
+
+    if (
+      mouseX > bx - boxSize &&
+      mouseX < bx + boxSize &&
+      mouseY > by - boxSize &&
+      mouseY < by + boxSize
+    ) {
+      overBox = true;
+      if (!locked) {
+        stroke(255);
+      }
+    } else {
+      overBox = false;
+    }
+
+    fill(255,255,255);
+    ellipse(bx, by, boxSize, boxSize);
+
+
+    textFont(mainFont);
+    textSize(windowWidth/10)
+    fill(255, 255, 255);
+    text("feed me", windowWidth*.05, windowHeight*.8-(windowWidth/15));
+
+    fill(128,128,128);
+    textFont(subFont);
+    textSize(windowWidth/20)
+    fill(128, 128, 128)
+    text("pink phantom", windowWidth*.07, windowHeight*.8);
+
+    if(song.isPlaying()){
+      image(pauseIcon, (windowWidth/2)-50, windowHeight*.85, 100, 100);
+    }else{
+      image(playIcon, (windowWidth/2)-50, windowHeight*.85, 100, 100);
+    }
+
   }else{
-    image(playIcon, (windowWidth/2)-100, windowHeight-300, 200, 200);
+    clear();
+    background(0,0,0,0)
+    fadeAway = true;
   }
 
 }
 
 
 function mousePressed() {
+console.log(mouseX, mouseY)
+
+if(mouseY<aboveSongTitle){
+  if(opacityBoi == true){
+    opacityBoi = false;
+  }else{
+    opacityBoi = true;
+  }
+}
+
+  if (overBox) {
+    boxSize = big;
+    locked = true;
+    fill(255, 255, 255);
+  } else {
+    locked = false;
+  }
+  xOffset = mouseX - bx;
+
+
+  if(
+    mouseX>314 &&
+    mouseX<414 &&
+    mouseY>615 &&
+    mouseY<717){
+
   if (song.isPlaying()) {
     // .isPlaying() returns a boolean
-    song.stop();
-  } else {
+    song.pause();
+  } else{
     song.play();
   }
+}
+
+}
+
+function mouseDragged() {
+  if(bx>=50 && bx<=675){
+
+  if (locked) {
+    bx = mouseX - xOffset;
+  }
+}else{
+  locked = false;
+  if(mouseX<50){
+    bx = 50;
+  }
+  if(mouseX>675){
+    bx = 675;
+  }
+}
+}
+
+function mouseReleased() {
+  locked = false;
+  boxSize = small;
 }
